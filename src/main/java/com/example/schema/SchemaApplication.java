3dbee7ffb5b712473b4417ab7e2856e4327a2f9b
RahulKumarMishra <kumarrahul.allduniv@gmail.com>
2022-10-18 00:53:38 +0530

    created project

--- src/main/java/com/example/schema/SchemaApplication.java
+++ src/main/java/com/example/schema/SchemaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SchemaApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SchemaApplication.class, args);
     }
 
