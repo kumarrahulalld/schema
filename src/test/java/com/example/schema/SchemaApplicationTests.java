b986dda9963e3912cc416d12bd8fb8f1022edc85
RahulKumarMishra <kumarrahul.allduniv@gmail.com>
2022-10-18 00:51:21 +0530

    created project

---
+++ src/test/java/com/example/schema/SchemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
